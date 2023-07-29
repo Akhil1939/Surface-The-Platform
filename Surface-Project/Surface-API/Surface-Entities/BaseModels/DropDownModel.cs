c3083e5b49be8fea88748fb395372eb3249b4729
Smit-D <smit4049@gmail.com>
2023-07-29 16:27:34 +0530

    Inital Migration: Basemodel: DdnModel, Basemodel Tables: Role,Status,StatusGroup,User,LoginProvider Added

---
+++ Surface-Project/Surface-API/Surface-Entities/BaseModels/DropDownModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Surface_Entities.Entities
+{
+    public class DropDownModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+        public bool IsActive { get; set; }
+    }
+}
