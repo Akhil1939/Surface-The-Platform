c5669eaff99693c0a2862e62c2da8c1b5568810e
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-07-31 09:16:29 +0530

    commit

---
+++ Surface-Project/Surface-API/Surface-API/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Surface_API.Areas.Admin.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // GET: api/<AccountController>
+        [HttpPost("register")]
+
+        public IActionResult Register()
+        {
+            return Ok("register done!");
+        }
+    }
+}
