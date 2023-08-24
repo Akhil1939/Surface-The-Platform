53c6dcb113b6c66feda32b3eb9e73765fc845d6e
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-08-24 13:46:05 +0530

    commit

--- Surface-Project/Surface-UI/src/app/app.module.ts
+++ Surface-Project/Surface-UI/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment.development';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ 
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule
