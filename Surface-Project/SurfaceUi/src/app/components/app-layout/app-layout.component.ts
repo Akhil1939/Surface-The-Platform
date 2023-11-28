b58e5f8ad28d09a92c11766992457a46134b689d
AKhil Kukadiya <73292704+Akhil1939@users.noreply.github.com>
2023-11-28 21:17:04 +0530

    sidebar refine

--- Surface-Project/SurfaceUi/src/app/components/app-layout/app-layout.component.ts
+++ Surface-Project/SurfaceUi/src/app/components/app-layout/app-layout.component.ts
@@ -1,42 +1,59 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, RouterOutlet],
+  imports: [CommonModule, SidebarComponent, RouterOutlet, HeaderComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.css'
 })
 export class AppLayoutComponent {
   @ViewChild('body') body!: ElementRef;
   @ViewChild('sidebar') sidebar!: ElementRef;
-  @ViewChild('toggle') toggle!: ElementRef;
-  @ViewChild('searchBtn') searchBtn!: ElementRef;
-  @ViewChild('modeSwitch') modeSwitch!: ElementRef;
-  @ViewChild('modeText') modeText!: ElementRef;
+@ViewChild(SidebarComponent) sidebarComponent:SidebarComponent |undefined ;
 
   ngAfterViewInit() {
-    this.toggle.nativeElement.addEventListener("click", () => {
-      this.sidebar.nativeElement.classList.toggle("close");
-    });
+    console.log("body",this.body);
+    if (this.sidebarComponent) {
+       
+     const toggle:ElementRef|undefined = this.sidebarComponent.toggle;
+     const searchBtn :ElementRef|undefined = this.sidebarComponent.searchBtn;
+     const modeSwitch :ElementRef|undefined = this.sidebarComponent.modeSwitch;
+     const modeText:ElementRef|undefined = this.sidebarComponent.modeText;
 
-    this.searchBtn.nativeElement.addEventListener("click", () => {
-      console.log("search")
-      this.sidebar.nativeElement.classList.remove("close");
-    });
 
-    this.modeSwitch.nativeElement.addEventListener("click", () => {
-      console.log("mode clicked")
-      this.body.nativeElement.classList.toggle("dark");
-      console.log("change mode to dark")
-      if (this.body.nativeElement.classList.contains("dark")) {
-        this.modeText.nativeElement.innerText = "Light mode";
-      } else {
-        this.modeText.nativeElement.innerText = "Dark mode";
-      }
-    });
+     toggle.nativeElement.addEventListener("click", () => {
+       this.sidebar?.nativeElement.classList.toggle("close");
+     });
+ 
+     searchBtn.nativeElement.addEventListener("click", () => {
+       console.log("search")
+       this.sidebar?.nativeElement.classList.remove("close");
+     });
+ 
+     modeSwitch?.nativeElement.addEventListener("click", () => {
+       console.log("mode clicked")
+       this.body.nativeElement.classList.toggle("dark");
+       console.log("change mode to dark")
+       if (this.body.nativeElement.classList.contains("dark")) {
+         modeText.nativeElement.innerText = "Light mode";
+       } else {
+         modeText.nativeElement.innerText = "Dark mode";
+       }
+     });
+    }
+
+
+
+
+
+
+
+
+
   }
 }
