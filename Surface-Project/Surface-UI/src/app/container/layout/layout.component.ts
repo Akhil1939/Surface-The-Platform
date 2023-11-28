6a705f98df1532ae680818e43a69589229aa4b3e
AKhil Kukadiya <akhilkukadiya464@gmail.com>
2023-11-28 08:14:36 +0530

    angular 17 setup

--- Surface-Project/Surface-UI/src/app/container/layout/layout.component.ts
+++ Surface-Project/Surface-UI/src/app/container/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
+  @ViewChild('body') body!: ElementRef;
+  @ViewChild('sidebar') sidebar!: ElementRef;
+  @ViewChild('toggle') toggle!: ElementRef;
+  @ViewChild('searchBtn') searchBtn!: ElementRef;
+  @ViewChild('modeSwitch') modeSwitch!: ElementRef;
+  @ViewChild('modeText') modeText!: ElementRef;
 
+  ngAfterViewInit() {
+    this.toggle.nativeElement.addEventListener("click", () => {
+      this.sidebar.nativeElement.classList.toggle("close");
+    });
+
+    this.searchBtn.nativeElement.addEventListener("click", () => {
+      console.log("search")
+      this.sidebar.nativeElement.classList.remove("close");
+    });
+
+    this.modeSwitch.nativeElement.addEventListener("click", () => {
+      console.log("mode clicked")
+      this.body.nativeElement.classList.toggle("dark");
+      console.log("change mode to dark")
+      if (this.body.nativeElement.classList.contains("dark")) {
+        this.modeText.nativeElement.innerText = "Light mode";
+      } else {
+        this.modeText.nativeElement.innerText = "Dark mode";
+      }
+    });
+  }
 }
